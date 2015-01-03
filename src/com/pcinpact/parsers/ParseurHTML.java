36b4254e3c730add9f17c3d82b81112503c16d74
Anael Mobilia <contrib@anael.eu>
2015-01-03 17:27:34 +0100

    archi nouveau parser

---
+++ src/com/pcinpact/parsers/ParseurHTML.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2015 Anael Mobilia
+ * 
+ * This file is part of NextINpact-Unofficial.
+ * 
+ * NextINpact-Unofficial is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * NextINpact-Unofficial is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with NextINpact-Unofficial. If not, see <http://www.gnu.org/licenses/>
+ */
+package com.pcinpact.parsers;
+
+import java.util.ArrayList;
+
+import com.pcinpact.items.ArticleItem;
+import com.pcinpact.items.CommentaireItem;
+
+public class ParseurHTML {
+
+	public ArrayList<ArticleItem> getListeArticles(String monInput) {
+		return null;
+	}
+
+	public ArticleItem getArticle(String monInput) {
+		return null;
+	}
+
+	public ArrayList<CommentaireItem> getCommentaires(String monInput) {
+		return null;
+	}
+}
