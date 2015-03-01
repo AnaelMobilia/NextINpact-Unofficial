6ed9f8da843b4752d91586c270287aae532714fb
Anael Mobilia <contrib@anael.eu>
2015-03-01 20:53:00 +0100

    Nouvelle méthode de gestion de DL des contenus abonnés (stub) #167

---
+++ src/com/pcinpact/downloaders/compteAbonne.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2015 Anael Mobilia
+ * 
+ * This file is part of NextINpact-Unofficial.
+ * 
+ * NextINpact-Unofficial is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * NextINpact-Unofficial is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with NextINpact-Unofficial. If not, see <http://www.gnu.org/licenses/>
+ */
+package com.pcinpact.downloaders;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+
+/**
+ * Connexion au compte abonné et gestion du DL des articles abonnés
+ * @author Anael
+ *
+ */
+public class compteAbonne {
+	private ArrayList<String> urls;
+	
+	
+	/**
+	 * Télécharge un article "Abonné"
+	 * @return
+	 */
+	public static byte[] downloadArticleAbonne(final String uneURL, final Context unContext, boolean compression, boolean contenuAbonne, boolean uniquementSiConnecte) {
+		
+		return null;
+	}
+}
