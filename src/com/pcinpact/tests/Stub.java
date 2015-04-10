cb26cd73cc155f62ecdea99c08f8979d29bd8eef
Anael Mobilia <contrib@anael.eu>
2015-04-10 03:43:46 +0200

    Mise en place junit (stub)

---
+++ src/com/pcinpact/tests/Stub.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.pcinpact.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Anael
+ *
+ */
+public class Stub {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
