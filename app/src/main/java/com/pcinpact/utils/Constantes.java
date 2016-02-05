b61d8a5c628fdea2fa1177a8365ca287ad14e002
Anael Mobilia <contrib@anael.eu>
2016-02-05 23:45:17 +0100

    Passage en 2.0.2 dev

--- app/src/main/java/com/pcinpact/utils/Constantes.java
+++ app/src/main/java/com/pcinpact/utils/Constantes.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Anael Mobilia
+ * Copyright 2015, 2016 Anael Mobilia
  * 
  * This file is part of NextINpact-Unofficial.
  * 
@@ -36,7 +36,7 @@ public class Constantes {
     /**
      * MODE DEBUG.
      */
-    public static final Boolean DEBUG = false;
+    public static final Boolean DEBUG = true;
     /**
      * Contact du développeur
      */
