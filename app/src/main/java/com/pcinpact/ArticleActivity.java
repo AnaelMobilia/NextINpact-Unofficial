db36f53a873f9e14325234a37d5749a8776b3560
Anael Mobilia <contrib@anael.eu>
2016-09-19 06:18:24 +0200

    backport 99df1694fbe92a7932c7be9e7241485dcfd8d9c3#diff-8e15eab060aaff03545d16f55225090a

--- app/src/main/java/com/pcinpact/ArticleActivity.java
+++ app/src/main/java/com/pcinpact/ArticleActivity.java
@@ -30,18 +30,14 @@ import android.view.MenuItem;
 
 import com.pcinpact.datastorage.DAO;
 import com.pcinpact.items.ArticleItem;
-import com.pcinpact.items.Item;
-import com.pcinpact.network.RefreshDisplayInterface;
 import com.pcinpact.utils.Constantes;
-
-import java.util.ArrayList;
 
 /**
  * Affichage d'un article.
  *
  * @author Anael
  */
-public class ArticleActivity extends AppCompatActivity implements RefreshDisplayInterface {
+public class ArticleActivity extends AppCompatActivity {
     /**
      * ID de l'article actuel.
      */
@@ -156,15 +152,6 @@ public class ArticleActivity extends AppCompatActivity implements RefreshDisplay
         }
 
         return super.onOptionsItemSelected(pItem);
-    }
-
-    @Override
-    public void downloadHTMLFini(final String uneURL, final ArrayList<? extends Item> desItems) {
-    }
-
-    @Override
-    public void downloadImageFini(final String uneURL) {
-        // Aucune action.
     }
 
     /**
